c8b319680e9a8002bbe32ecb7e1949c9660db017
ESiler <eleanor.siler@gmail.com>
2024-08-17 17:09:37 -0400

    color coded maps; yolo automation

--- 02_shuffle_files_for_yolo.sh
+++ 02_shuffle_files_for_yolo.sh
@@ -2,14 +2,18 @@
 
 echo "Welcome to this YOLO file shuffling script!"
 
-echo "What is your training zones? Enter zone_A, zone_B, etc."
+echo "Which plot labels are you using? eg 1_cat, 2_conifer_cat, 11_genius_cat: "
+read label_set
+
+echo "What are your training zones (eg zone_A, zone_B, etc.)? Enter your training zones separated by spaces: "
 read train_zone
 
-echo "What is your validation zone? Enter zone_A, zone_B, etc."
+echo "What are your validation zones(eg zone_A, zone_B)? Enter your validation zones separated by spaces: "
 read val_zone
 
+
 # Label directories
-source_dir_l="../../../Plot_Labels/train"
+source_dir_l="../../../Plot_Labels/${label_set}"
 destination_l_t="../YOLO_DATASET_TMP/labels/train"
 destination_l_v="../YOLO_DATASET_TMP/labels/val"
 
@@ -25,39 +29,47 @@ rm -f ${destination_l_v}/*
 rm -f ${destination_p_t}/*
 rm -f ${destination_p_v}/*
 
+#For each training zone:
+for z in ${train_zone[@]}
+do
 # Create symlinks for each line in the training zone file
-while IFS= read -r line
+    while IFS= read -r line
+    do
+      # Construct the source and destination file paths for training labels
+      source_file="${source_dir_l}/${line}.txt"
+      destination_file="${destination_l_t}/${line}.txt"
+      # Create the symlink for training labels
+      ln -s "$source_file" "$destination_file"
+
+      # Construct the source and destination file paths for training images
+      source_image="${source_dir_p}/${line}.png"  
+      destination_image="${destination_p_t}/${line}.png"
+      # Create the symlink for training images
+      ln -s "$source_image" "$destination_image"
+    done < "../Zone_Plots/$z.txt"
+done
+
+#For each validation zone:
+for z in ${val_zone[@]}
 do
-  # Construct the source and destination file paths for training labels
-  source_file="${source_dir_l}/${line}.txt"
-  destination_file="${destination_l_t}/${line}.txt"
-  # Create the symlink for training labels
-  ln -s "$source_file" "$destination_file"
+    # Create symlinks for each line in the validation zone file
+    while IFS= read -r line
+    do
+      # Construct the source and destination file paths for validation labels
+      source_file="${source_dir_l}/${line}.txt"
+      destination_file="${destination_l_v}/${line}.txt"
+      # Create the symlink for validation labels
+      ln -s "$source_file" "$destination_file"
 
-  # Construct the source and destination file paths for training images
-  source_image="${source_dir_p}/${line}.png"  
-  destination_image="${destination_p_t}/${line}.png"
-  # Create the symlink for training images
-  ln -s "$source_image" "$destination_image"
-done < "../Zone_Plots/${train_zone}.txt"
-
-# Create symlinks for each line in the validation zone file
-while IFS= read -r line
-do
-  # Construct the source and destination file paths for validation labels
-  source_file="${source_dir_l_v}/${line}.txt"
-  destination_file="${destination_l_v}/${line}.txt"
-  # Create the symlink for validation labels
-  ln -s "$source_file" "$destination_file"
-
-  # Construct the source and destination file paths for validation images
-  source_image="${source_dir_p_v}/${line}.png" 
-  destination_image="${destination_p_v}/${line}.png"
-  # Create the symlink for validation images
-  ln -s "$source_image" "$destination_image"
-done < "../Zone_Plots/${val_zone}.txt"
+      # Construct the source and destination file paths for validation images
+      source_image="${source_dir_p}/${line}.png" 
+      destination_image="${destination_p_v}/${line}.png"
+      # Create the symlink for validation images
+      ln -s "$source_image" "$destination_image"
+    done < "../Zone_Plots/$z.txt"
+done
 
 echo "Thanks! Your training zones are $train_zone and your validation zones are $val_zone"
-echo "Your data has been prepped to run YOLO!"
+echo "Your data has been prepped to run YOLO! Please verify that your symlinks work before proceeding."
 
 
--- Y1_run_yolo.py
+++ Y1_run_yolo.py
@@ -5,7 +5,7 @@ from ultralytics import YOLO
 #get data
 config = input('Enter YAML file, eg ../YOLO_DATASET_TMP/data_config.yaml: ')
 results_file = input('Enter results file location, eg ../YOLO_results: ')
-project_name = input('Enter a name for this run. This will be the name of your results folder.')
+project_name = input('Enter a name for this run. This will be the name of your results folder: ')
 
 #get model
 model_version = input('Enter yolo model path, eg yolo_models/yolov8n.pt: ')
@@ -16,6 +16,6 @@ results = model.train(data=config,
                       epochs=100, 
                       imgsz=218,
                       project=results_file,
-                      name=project_name
+                      name=project_name,
                       plots=True
                       )
